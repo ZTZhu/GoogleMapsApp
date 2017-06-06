26e76e6d335f7abf5c7ca3f32adcfc3c2f69c2f3
ZTZhu <ZhuZ5918@sduhsd.lan>
2017-06-06 09:30:14 -0700

    Minor Change
    Task 6 needs fixing

--- GoogleMapsApp/app/src/main/java/com/example/zhuz5918/googlemapsapp/MapsActivity.java
+++ GoogleMapsApp/app/src/main/java/com/example/zhuz5918/googlemapsapp/MapsActivity.java
@@ -321,6 +321,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     };
 
     public void track(View v) {
+        tracked = true;
         if (tracked == true) {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 // TODO: Consider calling
